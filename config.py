83c794e51c490422fc80b12804dfa20d64673fc9
BettercallYsh <166407864+BettercallYsh@users.noreply.github.com>
2025-06-08 12:51:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,19 +18,19 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7s")
-APP_ID = int(os.environ.get("APP_ID", "24938067")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8115790377:AAFjV4EjLzCjviVNhp5hUIki4JKQrel-McQ")
+APP_ID = int(os.environ.get("APP_ID", "20841767")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "9a0aec111e02a302f74ad535bd98440a") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002131461426")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "") # Owner username without @
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002428905407 ")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "apatheticyash") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "1439206175")) # Owner id
 #--------------------------------------------
-PORT = os.environ.get("PORT", "8098")
+PORT = os.environ.get("PORT", "9777")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongoddb.net/")
-DB_NAME = os.environ.get("DATABASE_NAME", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://harshsharma:harsh@harshsharma.2jstkwo.mongodb.net/")
+DB_NAME = os.environ.get("DATABASE_NAME", "earning")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/iblamedante")
